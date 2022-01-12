0ffbf14229254f350a8fc4814c22c528ca265473
mrsterner <simon@sterner.dev>
2022-01-12 16:15:32 +0100

    Cleaned up the files

--- src/main/java/net/zestyblaze/lycanthropy/common/registry/LycanthropyConfiguredStructuresInit.java
+++ src/main/java/net/zestyblaze/lycanthropy/common/registry/LycanthropyConfiguredStructuresInit.java
@@ -23,16 +23,7 @@ public class LycanthropyConfiguredStructuresInit {
     }
 
     public static void addStructureSpawningToDimensionsAndBiomes() {
-
-        /*
-         * This is the API you will use to add anything to any biome.
-         * This includes spawns, changing the biome's looks, messing with its temperature,
-         * adding carvers, spawning new features... etc
-         */
         BiomeModifications.addStructure(
-        // Add our structure to all biomes that have any of these biome categories. This includes modded biomes.
-        // You can filter to certain biomes based on stuff like temperature, scale, precipitation, mod id, etc.
-        // See BiomeSelectors's methods for more options or write your own by doing `(context) -> context.whatever() == condition`
         BiomeSelectors.categories(
         Biome.Category.DESERT,
         Biome.Category.EXTREME_HILLS,
@@ -42,8 +33,6 @@ public class LycanthropyConfiguredStructuresInit {
         Biome.Category.PLAINS,
         Biome.Category.SAVANNA,
         Biome.Category.TAIGA),
-        // The registry key of our ConfiguredStructure so BiomeModification API can grab it
-        // later to tell the game which biomes that your structure can spawn within.
         RegistryKey.of(
         Registry.CONFIGURED_STRUCTURE_FEATURE_KEY,
         BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE.getId(LycanthropyConfiguredStructuresInit.CONFIGURED_HUNTERS_HOUSE))
--- src/main/java/net/zestyblaze/lycanthropy/common/world/structures/HunterHouseStructure.java
+++ src/main/java/net/zestyblaze/lycanthropy/common/world/structures/HunterHouseStructure.java
@@ -24,7 +24,6 @@ import java.util.Optional;
 
 public class HunterHouseStructure extends StructureFeature<StructurePoolFeatureConfig> {
     public HunterHouseStructure(Codec<StructurePoolFeatureConfig> codec) {
-        // Create the pieces layout of the structure and give it to the game
         super(codec, HunterHouseStructure::createPiecesGenerator, PostPlacementProcessor.EMPTY);
     }
     public static final Pool<SpawnSettings.SpawnEntry> STRUCTURE_MONSTERS = Pool.of(
@@ -57,31 +56,13 @@ public class HunterHouseStructure extends StructureFeature<StructurePoolFeatureC
     }
 
     public static Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> createPiecesGenerator(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
-
-        // Check if the spot is valid for our structure. This is just as another method for cleanness.
-        // Returning an empty optional tells the game to skip this spot as it will not generate the structure.
         if (!HunterHouseStructure.isFeatureChunk(context)) {
             return Optional.empty();
         }
 
-        /*
-         * The only reason we are using StructurePoolFeatureConfig here is that further down, we are using
-         * StructurePoolBasedGenerator.generate which requires StructurePoolFeatureConfig. However, if you create your own
-         * StructurePoolBasedGenerator.generate, you could reduce the amount of workarounds like above that you need
-         * and give yourself more opportunities and control over your structures.
-         *
-         * An example of a custom StructurePoolBasedGenerator.generate in action can be found here (warning, it is using Mojmap mappings):
-         * https://github.com/TelepathicGrunt/RepurposedStructures-Fabric/blob/1.18/src/main/java/com/telepathicgrunt/repurposedstructures/world/structures/pieces/PieceLimitedJigsawManager.java
-         */
         StructurePoolFeatureConfig newConfig = new StructurePoolFeatureConfig(
         () -> context.registryManager().get(Registry.STRUCTURE_POOL_KEY)
-        .get(new Identifier(Lycanthropy.MODID, "hunter_house/start_pool")),
-
-        // How many pieces outward from center can a recursive jigsaw structure spawn.
-        // Our structure is only 1 piece outward and isn't recursive so any value of 1 or more doesn't change anything.
-        // However, I recommend you keep this a decent value like 7 so people can use datapacks to add additional pieces to your structure easily.
-        // But don't make it too large for recursive structures like villages or you'll crash server due to hundreds of pieces attempting to generate!
-        10);
+        .get(new Identifier(Lycanthropy.MODID, "hunter_house/start_pool")), 10);
 
         StructureGeneratorFactory.Context<StructurePoolFeatureConfig> newContext = new StructureGeneratorFactory.Context<>(
         context.chunkGenerator(),
@@ -95,7 +76,6 @@ public class HunterHouseStructure extends StructureFeature<StructurePoolFeatureC
         context.registryManager()
         );
 
-        // Turns the chunk coordinates into actual coordinates we can use. (Gets center of that chunk)
         BlockPos blockpos = context.chunkPos().getCenterAtY(0);
 
         Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> structurePiecesGenerator =
@@ -108,17 +88,9 @@ public class HunterHouseStructure extends StructureFeature<StructurePoolFeatureC
         true // Place at heightmap (top land). Set this to false for structure to be place at the passed in blockpos's Y value instead.
         // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
         );
-        /*
-         * Note, you are always free to make your own StructurePoolBasedGenerator class and implementation of how the structure
-         * should generate. It is tricky but extremely powerful if you are doing something that vanilla's jigsaw system cannot do.
-         * Such as for example, forcing 3 pieces to always spawn every time, limiting how often a piece spawns, or remove the intersection limitation of pieces.
-         *
-         * An example of a custom StructurePoolBasedGenerator.generate in action can be found here (warning, it is using Mojmap mappings):
-         * https://github.com/TelepathicGrunt/RepurposedStructures-Fabric/blob/1.18/src/main/java/com/telepathicgrunt/repurposedstructures/world/structures/pieces/PieceLimitedJigsawManager.java
-         */
+
 
         if(structurePiecesGenerator.isPresent()) {
-            // I use to debug and quickly find out if the structure is spawning or not and where it is.
             // This is returning the coordinates of the center starting piece.
             Lycanthropy.LOGGER.log(Level.DEBUG, "Rundown House at " + blockpos);
         }
